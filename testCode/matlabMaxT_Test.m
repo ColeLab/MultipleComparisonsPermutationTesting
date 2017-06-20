15389df6856d98df1721d8d22dd6b7d651db4528
Takuya Ito <taku.ito1@gmail.com>
2017-06-20 18:36:10 -0400

    rename

---
+++ testCode/matlabMaxT_Test.m
@@ -0,0 +1,48 @@
+% Taku Ito
+% 6/16/2017
+
+% Test script to test code for permutation testing in matlab
+clear all
+clc
+
+addpath('../matlabCode/')
+
+%% Create data set
+numVoxels = 100;
+numSubjs = 20;
+nCond = 2;
+sigEffects = 20;
+effectAmp = 3;
+
+disp('Running simulation...')
+disp(['Running contrasts on ' num2str(numVoxels) '  voxels (' num2str(numVoxels) ' independent tests) with ' num2str(numSubjs) ' subjects'])
+disp([num2str(nCond) ' conditions, with ' num2str(sigEffects) ' significant real effets'])
+
+tic
+% Construct data set (already contrasted)
+dataSet = zeros(numVoxels,numSubjs,nCond);
+for vox=1:numVoxels
+    if vox <= sigEffects
+        dataSet(vox,:, 1) = randn(1,numSubjs) + effectAmp;
+        dataSet(vox,:, 2) = randn(1,numSubjs);
+    else
+        dataSet(vox,:, 1) = randn(1,numSubjs);
+        dataSet(vox,:, 2) = randn(1,numSubjs);
+    end
+end
+
+% Create contrast map
+contrastSet = dataSet(:,:,1) - dataSet(:,:,2);
+
+
+%% Run permutation test
+alpha = .05;
+[t, maxT_thresh] = maxT(contrastSet,'nullmean', 0, ...
+                              'alpha', alpha, ...
+                              'tail', 1, ...
+                              'permutations',10000, ...
+                              'nproc', 10);
+toc
+
+disp(['Number of true effects: ' num2str(sigEffects)])
+disp(['Number of statistically significant effects (p < .05): ' num2str(sum(t>maxT_thresh))])
